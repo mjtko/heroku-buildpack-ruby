49f33d733dcc789db3cda48db17c4f5a69fc79ee
Mark J. Titorenko <mark.titorenko@alces-software.com>
2013-02-01 20:11:34 +0000

    Try not to fail so much.

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -684,7 +684,8 @@ params = CGI.parse(uri.query || "")
 
       # end
 
-      env_vars       = "env BUNDLE_GEMFILE=#{pwd}/Gemfile BUNDLE_CONFIG=#{pwd}/.bundle/config CPATH=#{yaml_include}:$CPATH CPPATH=#{yaml_include}:$CPPATH LIBRARY_PATH=#{yaml_lib}:$LIBRARY_PATH RUBYOPT=\"#{syck_hack}\""
+      pwd            = run("pwd").chomp
+      env_vars       = "env BUNDLE_GEMFILE=#{pwd}/Gemfile BUNDLE_CONFIG=#{pwd}/.bundle/config RUBYOPT=\"#{syck_hack}\""
       puts "Running: #{bundle_command}"
       bundler_output << pipe("#{env_vars} #{bundle_command} --no-clean 2>&1")
 
